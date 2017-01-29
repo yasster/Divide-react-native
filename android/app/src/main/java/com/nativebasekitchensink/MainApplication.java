2022df0847f976a7d555d7f78b000fbb1eff291e
yasster <ymissoum1@gmail.com>
2017-01-29 00:03:42 -0500

    Add files via upload

---
+++ android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
@@ -0,0 +1,61 @@
+package com.nativebasekitchensink;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import fr.bamlab.rncameraroll.CameraRollPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new CameraRollPackage(),
+            new RCTCameraPackage(),
+            new VectorIconsPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+    public void onCreate() {
+      super.onCreate();
+      SoLoader.init(this, /* native exopackage */ false);
+    }
+ }
+
+
