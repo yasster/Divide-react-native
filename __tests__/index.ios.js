aa06fbac9304b59d7c7346fee352b580f33d08d1
yasster <ymissoum1@gmail.com>
2017-02-05 11:17:29 -0800

    Update index.ios.js

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -7,6 +7,7 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+    <setup />
+    ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
